a64905dc9ebba0e6a6e7aca526d116199a74eb55
eduardo <edspinheiro@gmail.com>
2022-08-23 00:13:35 -0300

    fix/Using lombok annotations to perform logging

--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -3,20 +3,18 @@ package com.polarbookshop.catalogservice.web;
 import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RequestMapping("/books")
 @RequiredArgsConstructor
 @RestController
 public class BookController {
-
-    private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
     private final BookService bookService;
 
