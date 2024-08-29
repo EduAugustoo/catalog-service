a7d523f06003ba0f6dde42e1f3ddcfab4b20ead6
EduAugustoo <edspinheiro@gmail.com>
2024-08-29 11:57:48 -0300

    Latest changes until section 5.4.2

--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -17,28 +17,34 @@ public class BookJsonTests {
 
     @Test
     void testSerialize() throws Exception {
-        var book = new Book("1234567890", "Title", "Author", 9.9);
+        var book = Book.of("1234567890", "Title", "Author", 9.9);
         var jsonContent = json.write(book);
 
+        assertThat(jsonContent).extractingJsonPathStringValue("@.id").isEqualTo(book.id());
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn").isEqualTo(book.isbn());
         assertThat(jsonContent).extractingJsonPathStringValue("@.title").isEqualTo(book.title());
         assertThat(jsonContent).extractingJsonPathStringValue("@.author").isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price").isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.created_date").isEqualTo(book.createdDate());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.last_modified_date").isEqualTo(book.lastModifiedDate());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.version").isEqualTo(book.version());
     }
 
     @Test
     void testDeserialize() throws Exception {
         var content = """
                  {
+                     "id": null,
                      "isbn": "1234567890",
                      "title": "Title",
                      "author": "Author",
-                     "price": 9.9
+                     "price": 9.9,
+                     "version": 0
                  }
                 """;
 
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(new Book("1234567890", "Title", "Author", 9.9));
+                .isEqualTo(Book.of("1234567890", "Title", "Author", 9.9));
     }
 }
